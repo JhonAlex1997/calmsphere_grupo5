e319c924f6c64be433590859c6f8114090f66871
JhonAlex1997 <jhonal97.jacj@gmail.com>
2025-09-19 17:44:19 -0500

    Rep V1.1.1

---
+++ calmsphere/ejercicio/src/main/java/pe/edu/upc/ejercicio/servicesinterfaces/Iejercicioservice.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.ejercicio.servicesinterfaces;
+
+import pe.edu.upc.ejercicio.entities.ejercicio;
+
+import java.util.List;
+
+public interface Iejercicioservice {
+    public List<ejercicio> list();
+}
