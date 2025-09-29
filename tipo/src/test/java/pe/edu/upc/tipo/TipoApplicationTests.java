2216e21fad63fa75df6c7551a78c3202994865fb
JhonAlex1997 <134982016+JhonAlex1997@users.noreply.github.com>
2025-09-29 14:22:27 -0500

    tabla tipo V1

---
+++ tipo/src/test/java/pe/edu/upc/tipo/TipoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.tipo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
