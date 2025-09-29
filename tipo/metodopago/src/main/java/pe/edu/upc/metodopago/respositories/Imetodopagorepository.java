da15ad481aa1dc3a6281ba2ff806b16a9d0ba2b3
JhonAlex1997 <134982016+JhonAlex1997@users.noreply.github.com>
2025-09-29 17:12:07 -0500

    tabla met pago(services)V1

---
+++ tipo/metodopago/src/main/java/pe/edu/upc/metodopago/respositories/Imetodopagorepository.java
@@ -0,0 +1,8 @@
+package pe.edu.upc.metodopago.respositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.upc.metodopago.entities.metodopago;
+
+public interface Imetodopagorepository extends JpaRepository <metodopago, Integer> {
+
+}
