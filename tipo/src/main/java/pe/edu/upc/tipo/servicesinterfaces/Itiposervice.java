b49b9d32974fa3a9198bd2ce8ab9015f5a010067
JhonAlex1997 <134982016+JhonAlex1997@users.noreply.github.com>
2025-09-29 14:25:07 -0500

    tabla tipo package service V1

---
+++ tipo/src/main/java/pe/edu/upc/tipo/servicesinterfaces/Itiposervice.java
@@ -0,0 +1,10 @@
+package pe.edu.upc.tipo.servicesinterfaces;
+
+import pe.edu.upc.tipo.entities.tipo;
+
+import java.util.List;
+
+public interface Itiposervice {
+
+    public List<tipo> list();
+}
