0664e3b97425e32ebffaefbf3b4886be5ef5da86
JhonAlex1997 <134982016+JhonAlex1997@users.noreply.github.com>
2025-09-29 16:43:26 -0500

    tabla tipo package(Contro)V1

--- tipo/src/main/java/pe/edu/upc/tipo/controllers/tipocontroller.java
+++ tipo/src/main/java/pe/edu/upc/tipo/controllers/tipocontroller.java
@@ -1,13 +1,11 @@
 package pe.edu.upc.tipo.controllers;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.edu.upc.tipo.dtos.tipoDTO;
-import pe.edu.upc.tipo.entities.tipo;
 import pe.edu.upc.tipo.servicesinterfaces.Itiposervice;
 
 import java.util.List;
@@ -24,19 +22,5 @@ public class tipocontroller {
             ModelMapper m = new ModelMapper();
             return m.map(x, tipoDTO.class);
         }).collect(Collectors.toList());
-    }
-
-    public void insertar(@RequestBody tipoDTO ins){
-        ModelMapper m = new ModelMapper();
-        tipo t = m.map(ins, tipo.class);
-        iC.insert(t);
-    }
-    public void eliminar(int id){
-        iC.delete(id);
-    }
-    public void actualizar(@RequestBody tipoDTO upd){
-        ModelMapper m = new ModelMapper();
-        tipo t = m.map(upd, tipo.class);
-        iC.update(t);
     }
 }
