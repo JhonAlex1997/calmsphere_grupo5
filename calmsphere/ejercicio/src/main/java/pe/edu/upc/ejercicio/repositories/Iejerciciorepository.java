7868db5e42a8dd979f820597b0ac2c498dc3195d
JhonAlex1997 <134982016+JhonAlex1997@users.noreply.github.com>
2025-09-29 14:22:16 -0500

    tabla tipo V1

--- calmsphere/ejercicio/src/main/java/pe/edu/upc/ejercicio/repositories/Iejerciciorepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.ejercicio.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.ejercicio.entities.ejercicio;
-
-@Repository
-public interface Iejerciciorepository extends JpaRepository<ejercicio, Integer> {
-}
