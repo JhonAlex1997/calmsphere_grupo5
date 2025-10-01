2a9a6723432c3965226e360c4adfe406e5b8873d
JhonAlex1997 <134982016+JhonAlex1997@users.noreply.github.com>
2025-10-01 16:16:47 -0500

    V1

--- tipo/metodopago/src/main/java/pe/edu/upc/metodopago/respositories/Imetodopagorepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.metodopago.respositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.metodopago.entities.metodopago;
-
-public interface Imetodopagorepository extends JpaRepository <metodopago, Integer> {
-
-}
