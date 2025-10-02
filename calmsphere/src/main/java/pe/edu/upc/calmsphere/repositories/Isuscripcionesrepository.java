7da250d960264f0494e0fc7911b550b3af76b5d1
JhonAlex1997 <134982016+JhonAlex1997@users.noreply.github.com>
2025-10-02 10:20:09 -0500

    duplicado sus

--- calmsphere/src/main/java/pe/edu/upc/calmsphere/repositories/Isuscripcionesrepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.calmsphere.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.calmsphere.entities.suscripciones;
-
-public interface Isuscripcionesrepository extends JpaRepository<suscripciones,Integer> {
-}
